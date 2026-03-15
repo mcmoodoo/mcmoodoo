e7c8d787b9664da0367f6044b8c0fe4fa7aae910
mcmoodoo <rashid.makhmud@gmail.com>
2026-03-15 08:36:21 -0500

    ingress rule added for port 3000

--- bastion/main.tf
+++ bastion/main.tf
@@ -41,6 +41,14 @@ resource "aws_security_group" "bastion_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "front-end"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
